f885b128048f4a148d624a602ae2108e06f00f9c
Watit Thammarat <tongnakub@hotmail.com>
2018-08-02 00:29:59 +0700

    finish #3

---
+++ dist/js/main.js
@@ -0,0 +1,25 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const menuItems = document.querySelectorAll('.nav-item');
+
+let showMenu = false;
+
+menuBtn.addEventListener('click', e => {
+  if (!showMenu) {
+    menuBtn.classList.add('close');
+    menu.classList.add('show');
+    menuNav.classList.add('show');
+    menuBranding.classList.add('show');
+    menuItems.forEach(i => i.classList.add('show'));
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove('close');
+    menu.classList.remove('show');
+    menuNav.classList.remove('show');
+    menuBranding.classList.remove('show');
+    menuItems.forEach(i => i.classList.remove('show'));
+    showMenu = false;
+  }
+});
